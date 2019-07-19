03ac466e5aa4f99154df0b51dd505915b42203dd
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-19 13:52:58 -0400

    fixed infiate loop

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,7 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        while not self.light_is_on(): #I can use the on and off light to stop my sorting loop
+        if not self.light_is_on(): #I can use the on and off light to stop my sorting loop
             self.set_light_on() #while its not on it has to be on
             while self.can_move_right():
                 self.swap_item()
@@ -113,6 +113,7 @@ class SortingRobot:
                     self.swap_item()
                     self.move_right()
                 if not self.light_is_on():
+                    self.set_light_on()
                     while self.can_move_left():
                         self.move_left()
 
